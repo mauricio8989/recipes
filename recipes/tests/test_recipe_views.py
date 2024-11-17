431b674ec7202e42ef128382c026ccdd0133a2ec
mauricio89 <mauriciosouza8989@gmail.com>
2024-11-17 10:21:08 -0300

    :heavy_check_mark: test: testing recipe model

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -51,8 +51,8 @@ class RecipesViewsTest(RecipeTestBase):
 
     def test_recipe_category_template_loads_recipes(self):
         needed_title = 'This is a category test'
-        self.make_recipe(title=needed_title)
-        response = self.client.get(reverse('recipes:category', kwargs={'category_id': 1}))  # noqa: E501
+        recipe = self.make_recipe(title=needed_title)
+        response = self.client.get(reverse('recipes:category', kwargs={'category_id': recipe.category.id}))  # noqa: E501
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
 
@@ -60,6 +60,13 @@ class RecipesViewsTest(RecipeTestBase):
         self.assertIn('10 minutos', content)
         self.assertIn('5 porções', content)
         self.assertEqual(len(response_context_recipes), 1)
+
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse(
+            'recipes:category', kwargs={'category_id': recipe.category.id}
+        ))
+        self.assertEqual(response.status_code, 404)
 
     def test_detail_view_returns_correct_template(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 3}))
@@ -72,3 +79,10 @@ class RecipesViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
 
         self.assertIn(needed_title, content)
+
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        recipe = self.make_recipe(is_published=False)
+        response = self.client.get(reverse(
+            'recipes:recipe', kwargs={'id': recipe.id}
+        ))
+        self.assertEqual(response.status_code, 404)
